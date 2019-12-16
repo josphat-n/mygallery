61a883a6a61edef79c84ac4cd4fa0b53e4a5049b
Josephat-n <josphatnjoroge254@gmail.com>
2019-12-17 00:58:36 +0300

    create a custom script file.

---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+//disp the container when the card is clicked.
+$(".card").click(function(){
+
+   
+   //display the image details
+   $(".ImageShow").css("display", "block");
+
+
+
+
+});//end of card click function    
