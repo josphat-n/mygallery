7096b53002f3272782b09c21228a8df8abd646bc
Josephat-n <josphatnjoroge254@gmail.com>
2019-12-16 10:46:28 +0300

    create a template folder and configure project settings to identify the app

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gal.apps.GalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
