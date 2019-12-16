8000c1fe2aa3c6bdd1b93b7b71f5a9293fd36b4b
Josephat-n <josphatnjoroge254@gmail.com>
2019-12-16 13:05:45 +0300

    create three classes for db models

--- gal/models.py
+++ gal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+   location_name =  models.CharField(max_length =30)   
+   
+class Category(models.Model):
+   category_name =  models.CharField(max_length =30)   
+   
+class Image(models.Model):
+   image_name = models.CharField(max_length =30)
+   image_description = models.CharField(max_length =2000)
+   image_location =models.ForeignKey(Location, on_delete=models.CASCADE,)
+   image_category =models.ForeignKey(Category, on_delete=models.CASCADE,)
+   
+
+   
