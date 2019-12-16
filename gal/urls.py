42c1ebb2e7e95ea3a3521d6c8cfe2ba2c1880ac4
Josephat-n <josphatnjoroge254@gmail.com>
2019-12-16 15:25:41 +0300

    add a functionality to upload images.

--- gal/urls.py
+++ gal/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 from django.urls import path
@@ -5,3 +7,7 @@ from django.urls import path
 urlpatterns = [
    path('', views.home, name ='gallery-home'),
 ]
+
+
+if settings.DEBUG:
+   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
