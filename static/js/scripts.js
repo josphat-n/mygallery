59ba89bf18d9eb9764052399a867613fe4c9134e
Josephat-n <josphatnjoroge254@gmail.com>
2020-03-17 23:27:47 +0300

    redefine the function to display a modal

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,6 @@
-//disp the container when the card is clicked.
-$(".card").click(function(){
-
-   
-   //display the image details
-   $(".ImageShow").css("display", "block");
-
-
-
-
-});//end of card click function    
+$(function() {
+   $('.img').on('click', function() {
+      $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+      $('#imagemodal').modal('show');   
+   });		
+});
